31fca7fe9d37e5596391641bfeb4b422ac2a950a
Arinadi <arinadi.nur@gmail.com>
2020-05-08 20:09:54 +0700

    finish command API

--- routes/api.php
+++ routes/api.php
@@ -18,16 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('devices', 'devicesAPIController');
+// Route::resource('devices', 'devicesAPIController');
 
-Route::resource('device_data', 'device_dataAPIController');
+// Route::resource('device_data', 'device_dataAPIController');
 
-Route::resource('device_data_categories', 'device_data_categoriesAPIController');
+// Route::resource('device_data_categories', 'device_data_categoriesAPIController');
 
 Route::resource('device_commands', 'device_commandAPIController');
 
-Route::resource('bot_users', 'bot_usersAPIController');
+// Route::resource('bot_users', 'bot_usersAPIController');
 
-Route::resource('bot_users_pair_devices', 'bot_users_pair_devicesAPIController');
+// Route::resource('bot_users_pair_devices', 'bot_users_pair_devicesAPIController');
 
-Route::resource('account_types', 'account_typesAPIController');
+// Route::resource('account_types', 'account_typesAPIController');
